49c90b1909d75cd0f001483002090383da3395b7
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2025-06-11 20:13:10 -0300

    feat: create signin controller #8h

---
+++ foodiary-api/src/application/controllers/auth/SignInController.ts
@@ -0,0 +1,34 @@
+import { Controller } from '@application/contracts/Controller';
+import { SignInUseCase } from '@application/usecases/auth/SignInUseCase';
+import { Injectable } from '@kernel/decorators/Injectable';
+import { Schema } from '@kernel/decorators/Schema';
+import { SignInBody, signInSchema } from './schemas/signinSchema';
+
+@Injectable()
+@Schema(signInSchema)
+export class SignInController extends Controller<SignInController.Response> {
+  constructor(private signinUseCase: SignInUseCase) {
+    super();
+  }
+
+  protected override async handle({ body }: Controller.Request<SignInBody> ): Promise<Controller.Response<SignInController.Response>> {
+    const { email, password } = body;
+
+    const { accessToken, refreshToken } = await this.signinUseCase.execute({ email, password });
+
+    return {
+      statusCode: 200,
+      body: {
+        accessToken,
+        refreshToken,
+      },
+    };
+  }
+}
+
+export namespace SignInController {
+  export type Response = {
+    accessToken: string
+    refreshToken: string
+  }
+}
